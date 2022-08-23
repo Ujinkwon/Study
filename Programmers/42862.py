dd2755dd5a074ae383bb73f47ab92226bdb42d89
Ujinkwon <dbwlsdl586@gmail.com>
2022-08-23 17:52:04 +0900

    0823

--- Programmers/42862.py
+++ Programmers/42862.py
@@ -1,15 +1,21 @@
-from audioop import reverse
-import re
-
 
 def solution(n, lost, reserve):
     answer = 0
     for i in lost:
-        if i in reverse:
-            reverse.remove(i)
-    print(reserve)
+        if i in reserve:
+            reserve.remove(i)
+            lost.remove(i)
+    for i in lost:
+        if i+1 in reserve:
+            reserve.remove(i+1)
+            answer += 1
+        elif i-1 in reserve:
+            reserve.remove(i-1)
+            answer += 1
+    answer += (n - len(lost))
     return answer
 
 print(solution(5, [2,4], [1, 3, 5]))
-print(solution(5, [2, 4], [2, 3]))
-print(solution(5, [3], [1]))
+print(solution(5, [2, 4], [3]))
+print(solution(3, [3], [1]))
+print(solution(5, [1, 3], [1]))
