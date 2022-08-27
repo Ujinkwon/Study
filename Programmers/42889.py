37f943dbc2d8b7d04416db9d5b3484b52e2d461b
Ujinkwon <dbwlsdl586@gmail.com>
2022-08-27 15:09:17 +0900

    0827

--- Programmers/42889.py
+++ Programmers/42889.py
@@ -5,11 +5,14 @@ def solution(N, stages):
     total = 0
     for i in stages:
         cnt[i-1] += 1
-
-    for i in range(N):
+    for i in range(max(stages)):
         human[i] = len(stages)-total
         total += cnt[i]
         ret.append(cnt[i] / human[i])
+    while len(ret) < N:
+        ret.append(0)
+    if len(ret) > N:
+        ret = ret[:N]
 
     for i in range(N):
         answer.append(ret.index(max(ret))+1)
@@ -18,3 +21,4 @@ def solution(N, stages):
 
 print(solution(5,[2, 1, 2, 6, 2, 4, 3, 3]))
 print(solution(4,[4,4,4,4,4]))
+print(solution(5,[3,3,4]))
