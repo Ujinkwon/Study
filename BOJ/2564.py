113704009e5354f96058915b916649c0ff199953
Ujinkwon <dbwlsdl586@gmail.com>
2022-08-29 00:54:11 +0900

    0829

--- BOJ/2564.py
+++ BOJ/2564.py
@@ -7,3 +7,28 @@ store = [list(map(int, input().split())) for _ in range(n)]
 d, x = map(int, input().split())
 total = 0
 
+
+for i in store:
+    if d == 1:
+        t = [1,2,3,c,r,i[1]]
+    elif d == 2:
+        t = [2,1,3,c,r,c-i[1]]
+    elif d == 3:
+        t = [3,4,1,r,c,i[1]]
+    elif d == 4:
+        t = [4,3,1,r,c,r-i[1]]
+
+    if i[0] == t[0]:
+        total += abs(x - i[1])
+    elif i[0] == t[1]:
+        total += abs(x - i[1]) + t[3]
+        if i[1] > x:
+            total += min(x, t[4]-i[1]) * 2
+        else:
+            total += min(t[4]-x, i[1]) * 2
+    elif i[0] == t[2]:
+        total += x + t[5]
+    else:
+        total += (t[4]-x) + t[5]
+
+print(total)
--- SWEA/1860.py
+++ SWEA/1860.py
@@ -10,27 +10,15 @@ sys.stdin = open('1860.txt')
 t = int(input())
 for tc in range(1, t+1):
     n, m, k = map(int, input().split())
-    narr = list(map(int, input().split()))
-    narr.sort()
-
-    if narr[0] % m:
-            narr[0] -= 1
-    total = ((narr[0]//m) * k) -1
-    if total < 0:
-        res = 'ImPossible'
-    else:   
-        res = 'Possible'
-        for i in range(1, n):
-            if narr[i] % m:
-                narr[i] -= 1
-            total += (((narr[i] - narr[i-1])//m) * k) -1
-            if total < 0:
-                res = 'Impossible'
-                break
-    
+    people = list(map(int, input().split()))
+    people.sort()
+    res = 'Possible'
+    for i in range(n):
+        cal = ((people[i] // m) * k - i - 1)
+        if cal < 0:
+            res = 'Impossible'
+            break
     print(f'#{tc} {res}')
-
-
 
 
     # ret = '#'+str(tc)+' '+str(res)+'\n'
