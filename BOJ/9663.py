948f025c03802f108a27539a5495633562b27cd4
Ujinkwon <dbwlsdl586@gmail.com>
2022-09-18 21:23:27 +0900

    0918

--- BOJ/9663.py
+++ BOJ/9663.py
@@ -5,13 +5,14 @@ def dfs(x):
     else:
         for i in range(n): 
             arr[x] = i
-            for i in range(x): 
-                if arr[x] == arr[i] or abs(arr[x] - arr[i]) == x - i:
+            for j in range(x):
+                if arr[x] == arr[j] or abs(arr[x] - arr[j]) == x - j:
                     break
             else:
                 dfs(x+1) 
 
-n = int(input())
+# n = int(input())
+n = 8
 arr = [0] * n
 cnt = 0
 dfs(0)
