f45123e65a135b74a7bccd273d7a424249d97000
Ujinkwon <dbwlsdl586@gmail.com>
2022-10-11 00:02:18 +0900

    1011

--- SWEA/4340.py
+++ SWEA/4340.py
@@ -1,33 +1,40 @@
 import sys
 sys.stdin = open('4340.txt')
 
+def dfs(i, j, type, cnt):
+    global min_value
+    if i == n-1 and j == n-1:
+        if cnt < min_value:
+            min_value = cnt
+    elif cnt >= min_value:
+        return
+    else:
+        dir = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        type = arr[i][j]
+        if type == 1:
+            d = [dir[0], dir[1]]
+        elif type == 2:
+            d = [dir[2], dir[3]]
+        elif type == 3:
+            d = [dir[0], dir[2]]
+        elif type == 4:
+            d = [dir[1], dir[2]]
+        elif type == 5:
+            d = [dir[1], dir[3]]
+        elif type == 6:
+            d = [dir[0], dir[3]]
+        for di, dj in d:
+            ni, nj = i+di, j+dj
+            if 0 <= nj < n and 0 <= ni < n and not visited[ni][nj]:
+                visited[ni][nj] = 1
+                dfs(ni, nj, cnt+1)
+                visited[ni][nj] = 0
+
+
 t = int(input())
 for tc in range(1, t+1):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]   
-    i, j = 0, 0
-    flag, cnt = 1, 0
-    while i != n-1 and j != n-1:
-        if arr[i][j] == 1 or arr[i][j] == 2:
-            if flag == 1:
-                i, j = i, j+1
-            elif flag == 2:
-                i, j = i+1, j
-            cnt += 1
-        else:
-            if flag == 1:
-                if arr[i+1][j] != 0:
-                    i, j = i+1, j
-                else:
-                    i, j = i-1, j
-                flag = 2
-            elif flag == 2:
-                if arr[i][j+1] != 0:
-                    i, j = i, j+1
-                else:
-                    i, j = i, j-1
-                flag = 1
-            cnt += 1
-        print(i, j)
-    print(cnt)
+    visited = [[0]*n for _ in range(n+1)]
+
     print(f'#{tc}')
