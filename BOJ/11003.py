a203a164e00db271d710e960d9f0b85a7bdf1533
Ujinkwon <dbwlsdl586@gmail.com>
2022-10-23 23:49:02 +0900

    1023

--- BOJ/11003.py
+++ BOJ/11003.py
@@ -6,19 +6,7 @@ sys.stdin = open('11003.txt')
 start = time.time()
 n, l = map(int, input().split())
 arr = list(map(int, input().split()))
-q = deque()
 
-for i in range(n):
-    # i-L+1 인덱스 이전인 것들을 pop해준다.
-    while q and q[0][0] <= i-l:
-        q.popleft()
-
-    # 들어갈 숫자보다 큰 것들은 전부 pop해준다.
-    while q and q[-1][1] >= arr[i]:
-        q.pop()
-
-    q.append((i, arr[i]))
-    print(q[0][1], end=' ')
 
 # res = []
 # for i in range(1, n+1):
@@ -28,7 +16,6 @@ for i in range(n):
 #     res.append(min(arr[s:i]))
 # print(*res)
 # print('time :', time.time() - start)
-
 
 
 # res = [arr[0], min(arr[0], arr[1])]
