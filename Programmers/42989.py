c89edb741ad82b833a6ef6b8f1e1e4d5f7dfa3f3
Ujinkwon <dbwlsdl586@gmail.com>
2022-11-08 18:00:33 +0900

    1108

--- Programmers/42989.py
+++ Programmers/42989.py
@@ -2,16 +2,28 @@ def solution(m, n, puddles):
     answer = 0
     q = [[0, 0, 0]]
     res = []
+    min_cnt = m*n
     while q:
         i, j, cnt = q.pop(0)
+        if min_cnt < cnt:
+            continue
         if i == n-1 and j == m-1:
-            res.append(cnt)
-        for di, dj in [[0, 1], [1, 0]]:
-            ni, nj = i+di, j+dj
-            if 0 <= ni < n and 0 <= nj < m and [nj+1, ni+1] not in puddles:
-                q.append([ni, nj, cnt+1])
-                
-    answer = res.count(min(res)) % 1000000007
+            if min_cnt == cnt:
+                res.append(cnt)
+            elif min_cnt > cnt:
+                res = [cnt]    
+                min_cnt = cnt
+        else:
+            for di, dj in [[0, 1], [1, 0]]:
+                ni, nj = i+di, j+dj
+                if 0 <= ni < n and 0 <= nj < m and [nj+1, ni+1] not in puddles:
+                    q.append([ni, nj, cnt+1])
+
+    cnt = len(res)
+    if cnt >= 1000000007:
+        answer = cnt % 1000000007
+    else:
+        answer = cnt
     return answer
 
 
--- Programmers/72412.py
+++ Programmers/72412.py
@@ -1,38 +1,39 @@
-# def solution(info, query):
-#     answer = []
-#     for i in query:
-#         temp = list(i.replace('and ', '').split())
-#         cnt = 0
-#         for j in info:
-#             arr = list(j.split())
-#             for k in range(4):
-#                 if temp[k] == '-':
-#                     continue
-#                 if temp[k] != arr[k]:
-#                     break
-#             else:
-#                 if int(temp[4]) <= int(arr[4]):
-#                     cnt += 1
-#         answer.append(cnt)
-#     return answer
-
-from itertools import  combinations
-
 def solution(info, query):
     answer = []
-    dic = dict()
-    score = []
-    arr = [[] for _ in range(len(info))]
-    for i in info:
-        temp = list(i.split())
-        if temp[-1] not in score:
-            score.append(temp[-1])
-        for k in range(1, 5):
-            arr[score.index(temp[-1])].append(list(combinations(temp[:4], k)))
-    for i in arr:
-        print(i)
-    print(score)
+    for i in query:
+        temp = list(i.replace('and ', '').split())
+        cnt = 0
+        for j in info:
+            arr = list(j.split())
+            for k in range(4):
+                if temp[k] == '-':
+                    continue
+                if temp[k] != arr[k]:
+                    break
+            else:
+                if int(temp[4]) <= int(arr[4]):
+                    cnt += 1
+        answer.append(cnt)
     return answer
+
+# from itertools import  combinations
+
+# def solution(info, query):
+#     answer = [0]*len(info)
+#     arr = []
+#     for i in query:
+#         temp = tuple(i.replace('and ', '').replace('-', '').split())
+#         arr.append(temp)
+
+#     for i in range(len(arr)):
+#         score = int(arr[i][-1])
+#         a = arr[i][:len(arr[i])-1]
+#         for j in info:
+#             temp = list(j.split())
+#             for k in list(combinations(temp, len(arr[i]))):
+#                 if k[-1].isdigit() and int(k[-1]) >= score and k[:len(k)-1] == a:
+#                     answer[i] += 1
+#     return answer
 
 
 print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
