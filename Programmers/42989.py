f6078524870ebef1606cad452bc29dde554e7785
Ujinkwon <dbwlsdl586@gmail.com>
2022-11-08 16:42:19 +0900

    1108

--- Programmers/42989.py
+++ Programmers/42989.py
@@ -8,8 +8,9 @@ def solution(m, n, puddles):
             res.append(cnt)
         for di, dj in [[0, 1], [1, 0]]:
             ni, nj = i+di, j+dj
-            if 0 <= ni < n and 0 <= nj < m and [ni, nj] not in puddles:
+            if 0 <= ni < n and 0 <= nj < m and [nj+1, ni+1] not in puddles:
                 q.append([ni, nj, cnt+1])
+                
     answer = res.count(min(res)) % 1000000007
     return answer
 
